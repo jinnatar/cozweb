48b23d5b19e1482dec741fd65665d36c3ae219f1
Artanicus <artanicus@nocturnal.fi>
2019-05-01 20:07:11 +0300

    Improved structure

--- cozweb/__init__.py
+++ cozweb/__init__.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+
 __version__ = "0.0.1"
+
+from flask import Flask
+app = Flask(__name__)
+app.config.from_object('websiteconfig')
+
+@app.route("/")
+def root():
+    return "Index"
